2781f10367a2a95cbba1d14a1345afffe86dbd23
D  Yan <2907762730@qq.com>
2026-02-12 22:11:36 +0800

    opengeign 接入前3

---
+++ taopiaopiao-session-service/taopiaopiao-session-service-application/src/main/java/com/duanyan/taopiaopiao/sessionservice/application/client/EventClient.java
@@ -0,0 +1,13 @@
+package com.duanyan.taopiaopiao.sessionservice.application.client;
+
+import com.duanyan.taopiaopiao.eventservice.api.dto.EventResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "event-service", path = "/admin/events")
+public interface EventClient {
+
+    @GetMapping("/{id}")
+    EventResponse getEventById(@PathVariable("id") Long id);
+}
---
+++ taopiaopiao-session-service/taopiaopiao-session-service-application/src/main/java/com/duanyan/taopiaopiao/sessionservice/application/client/VenueClient.java
@@ -0,0 +1,13 @@
+package com.duanyan.taopiaopiao.sessionservice.application.client;
+
+import com.duanyan.taopiaopiao.venueservice.api.dto.VenueResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "venue-service", path = "/admin/venues")
+public interface VenueClient {
+
+    @GetMapping("/{id}")
+    VenueResponse getVenueById(@PathVariable("id") Long id);
+}
